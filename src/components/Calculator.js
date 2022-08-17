15a0ef940865b2c83dcce10e17f3f29cb6657289
Alphy Gacheru <alphygacheru@gmail.com>
2022-08-17 10:57:36 +0300

    Add calculator component and style it

---
+++ src/components/Calculator.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="calculator">
+        <div className="calculator-container">
+          <div className="output">
+            <span>0</span>
+          </div>
+          <button type="button" className="bg-light-grey">
+            AC
+          </button>
+          <button type="button" className="bg-light-grey">
+            +/-
+          </button>
+          <button type="button" className="bg-light-grey">
+            %
+          </button>
+          <button type="button" className="operator">
+            &#247;
+          </button>
+          <button type="button" className="bg-light-grey">
+            7
+          </button>
+          <button type="button" className="bg-light-grey">
+            8
+          </button>
+          <button type="button" className="bg-light-grey">
+            9
+          </button>
+          <button type="button" className="operator">
+            &times;
+          </button>
+          <button type="button" className="bg-light-grey">
+            4
+          </button>
+          <button type="button" className="bg-light-grey">
+            5
+          </button>
+          <button type="button" className="bg-light-grey">
+            6
+          </button>
+          <button type="button" className="operator">
+            -
+          </button>
+          <button type="button" className="bg-light-grey">
+            1
+          </button>
+          <button type="button" className="bg-light-grey">
+            2
+          </button>
+          <button type="button" className="bg-light-grey">
+            3
+          </button>
+          <button type="button" className="operator">
+            +
+          </button>
+          <button type="button" className="span-2 bg-light-grey">
+            0
+          </button>
+          <button type="button" className="bg-light-grey">
+            .
+          </button>
+          <button type="button" className="operator">
+            =
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
