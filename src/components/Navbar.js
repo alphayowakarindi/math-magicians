b956423dcdc8c08e62af4b78dd3507440007d83c
Alphy Gacheru <alphygacheru@gmail.com>
2022-08-24 08:42:39 +0300

    Update navbar layout and style it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 function Navbar() {
   return (
     <div className="navbar">
-      <h1>Math Magicians</h1>
+      <h1><a href="/home">Math Magicians</a></h1>
       <ul>
-        <li>Home |</li>
-        <li>Calculator |</li>
-        <li>Quote |</li>
+        <li>
+          <a href="/home">Home |</a>
+        </li>
+        <li>
+          <a href="/calculator">Calculator |</a>
+        </li>
+        <li>
+          <a href="/auote">Quote |</a>
+        </li>
       </ul>
     </div>
   );
