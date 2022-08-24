9438a5bd176e7d2866ba8972c0a6f519196f1a06
Alphy Gacheru <alphygacheru@gmail.com>
2022-08-24 07:52:16 +0300

    Import BrowseRouter, Route and Switch in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
