8fc0e0e56d9d773496dee7e3b4c93851dafd271b
pamphilemkp <pamphilemkp@gmail.com>
2022-08-25 18:07:12 +0300

    add snapshot test for home and navbar

---
+++ src/components/__snapShotTest__/Navbar.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from '../Navbar';
+
+test('should render Navbar', ()=>{
+
+    const tree = render(
+        <Router>
+             <Navbar />
+        </Router>
+    )
+    expect (tree).toMatchSnapshot();
+
+})
